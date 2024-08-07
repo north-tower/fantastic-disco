999be8ad45addf271ad3a20815d81016ff68bb27
north-tower <muneshmyke@gmail.com>
2024-08-07 14:41:56 +0300

    47

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,20 @@
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
+import Next from "@/components/Next";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main >
-      <section>  <Header /></section>
-   <section>
+ 
+   <section className="snap-center">
 
    <Hero />
+
+   </section>
+   <section className="snap-center">
+
+   <Next />
 
    </section>
     </main>
---
+++ components/Next.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+  } from "@/components/ui/card"
+  
+function Next() {
+  return (
+    <div className='bg-gray-800'>
+                    hsfdfs
+    </div>
+  )
+}
+
+export default Next
